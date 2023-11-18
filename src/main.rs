40a836e278d82b90c5cd7e6c40c173549dab42ce
motxx <motohiko.ave@gmail.com>
2023-11-18 19:20:10 +0900

    [wip] runs on testnet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
-//use gl_client::tls::{Scheduler, Signer, TlsConfig};
+use anyhow::{Context, Result};
+use gl_client::signer::Signer;
+use std::path::Path;
+
+use gl_client::bitcoin::Network;
+use gl_client::scheduler::Scheduler;
+use gl_client::tls::TlsConfig;
 
 #[tokio::main]
-async fn main() {
-    //let tls = TlsConfig::new()?.identity(certificate, key);
-    //let signer = Signer(seed, Network::Bitcoin, tls);
-    //let scheduler = Scheduler::with(signer.node_id(), Network::Bitcoin, "uri", &tls).await?;
+async fn main() -> Result<()> {
+    let cert_path = Path::new("./gl-certs/client.crt");
+    let device_cert = std::fs::read(cert_path).with_context(|| "Failed to read client.crt")?;
+
+    let key_path = Path::new("./gl-certs/client-key.pem");
+    let device_key = std::fs::read(key_path).with_context(|| "Failed to read client_key.pem")?;
+
+    let tls = TlsConfig::new()
+        .with_context(|| "Failed to create TLS configuration")?
+        .identity(device_cert, device_key);
+
+    let mnemonic_path = Path::new("./gl-certs/signer_mnemonic.txt");
+    let mnemonic_words =
+        std::fs::read(mnemonic_path).with_context(|| "Failed to read mnemonic_.txt")?;
+    let signer = Signer::new(mnemonic_words, Network::Testnet, tls.clone())
+        .with_context(|| "Failed to create signer")?;
+    let node_id = signer.node_id();
+
+    let scheduler = Scheduler::new(node_id, Network::Testnet).await?;
+    let node: gl_client::node::ClnClient = scheduler.schedule(tls).await?;
+
+    Ok(())
 }
