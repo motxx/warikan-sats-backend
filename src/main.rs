8a586d0d9abbee9d1b051004b69041e2ad746588
motxx <motohiko.ave@gmail.com>
2024-01-05 20:27:01 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,46 @@
-use anyhow::{Context, Result};
-use gl_client::signer::Signer;
-use std::path::Path;
+use std::env;
 
-use gl_client::bitcoin::Network;
-use gl_client::scheduler::Scheduler;
-use gl_client::tls::TlsConfig;
+use anyhow::Result;
+use bip39::{Language, Mnemonic};
+use breez_sdk_core::{
+    BreezEvent, BreezServices, EnvironmentType, EventListener, GreenlightNodeConfig,
+};
+use dotenvy::dotenv;
+
+struct AppEventListener;
+
+impl EventListener for AppEventListener {
+    fn on_event(&self, e: BreezEvent) {
+        println!("Received event: {:?}", e);
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cert_path = Path::new("./gl-certs/client.crt");
-    let device_cert = std::fs::read(cert_path).with_context(|| "Failed to read client.crt")?;
+    dotenv().expect(".env file not found");
+    let api_key = env::var("BREEZ_API_KEY").expect("BREEZ_API_KEY not found in .env file");
+    let invite_code =
+        Some(env::var("BREEZ_INVITE_CODE").expect("BREEZ_INVITE_CODE not found in .env file"));
 
-    let key_path = Path::new("./gl-certs/client-key.pem");
-    let device_key = std::fs::read(key_path).with_context(|| "Failed to read client_key.pem")?;
+    let mnemonic = Mnemonic::generate_in(Language::English, 12)?;
+    let seed = mnemonic.to_seed("");
 
-    let tls = TlsConfig::new()
-        .with_context(|| "Failed to create TLS configuration")?
-        .identity(device_cert, device_key);
+    // Create the default config
+    let mut config = BreezServices::default_config(
+        EnvironmentType::Production,
+        api_key,
+        breez_sdk_core::NodeConfig::Greenlight {
+            config: GreenlightNodeConfig {
+                partner_credentials: None,
+                invite_code,
+            },
+        },
+    );
 
-    let mnemonic_path = Path::new("./gl-certs/signer_mnemonic.txt");
-    let mnemonic_words =
-        std::fs::read(mnemonic_path).with_context(|| "Failed to read mnemonic_.txt")?;
-    let signer = Signer::new(mnemonic_words, Network::Testnet, tls.clone())
-        .with_context(|| "Failed to create signer")?;
-    let node_id = signer.node_id();
+    // Customize the config object according to your needs
+    config.working_dir = "path to an existing directory".into();
 
-    let scheduler = Scheduler::new(node_id, Network::Testnet).await?;
-    let node: gl_client::node::ClnClient = scheduler.schedule(tls).await?;
-
+    // Connect to the Breez SDK make it ready for use
+    let sdk = BreezServices::connect(config, seed.to_vec(), Box::new(AppEventListener {})).await?;
     Ok(())
 }
