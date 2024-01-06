bbe534978ad41c785a50d4d99665037d709abe8c
motxx <motohiko.ave@gmail.com>
2024-01-06 22:30:48 +0900

    WIP:

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,12 @@ impl EventListener for AppEventListener {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv().expect(".env file not found");
-    let api_key = env::var("BREEZ_API_KEY").expect("BREEZ_API_KEY not found in .env file");
-    let invite_code =
-        Some(env::var("BREEZ_INVITE_CODE").expect("BREEZ_INVITE_CODE not found in .env file"));
-
     let mnemonic = Mnemonic::generate_in(Language::English, 12)?;
     let seed = mnemonic.to_seed("");
+
+    dotenv().expect(".env file not found");
+    let api_key: String = env::var("BREEZ_API_KEY").expect("BREEZ_API_KEY not found in .env file");
+    // let invite_code = Some(env::var("BREEZ_INVITE_CODE").expect("BREEZ_INVITE_CODE not found in .env file"));
 
     // Create the default config
     let mut config = BreezServices::default_config(
@@ -32,13 +31,13 @@ async fn main() -> Result<()> {
         breez_sdk_core::NodeConfig::Greenlight {
             config: GreenlightNodeConfig {
                 partner_credentials: None,
-                invite_code,
+                invite_code: None,
             },
         },
     );
 
     // Customize the config object according to your needs
-    config.working_dir = "path to an existing directory".into();
+    config.working_dir = "./work".into();
 
     // Connect to the Breez SDK make it ready for use
     let sdk = BreezServices::connect(config, seed.to_vec(), Box::new(AppEventListener {})).await?;
